7d1e58ce24ab19cb428d0341ca9850a4d171f260
swickape <patrick.swickard@amplyfigroup.com>
2023-06-19 19:20:32 -0400

    added my fake dict to add more words

--- parsedict.rb
+++ parsedict.rb
@@ -2,11 +2,30 @@ require 'json'
 
 text = File.readlines('cleancmu.txt')
 text2 = File.read('dict-old.json')
+text3 = File.readlines('fakecmudict.txt')
 
 sylhash = JSON.parse(text2)
 #print sylhash
 
 text.each do |thisline|
+  if thisline =~ /^\w/
+    thisarray = thisline.split(/\s+/)
+    thisword = thisarray[0]
+    sylcount = 0
+    thisarray.each do |thisdatum|
+      if thisdatum =~ /(\d)$/
+        sylcount += 1
+      end
+    end
+    if not sylhash[thisword.downcase]
+      #print("hi #{thisword}\n")
+      sylhash[thisword.downcase] = sylcount
+    else
+      #print("innit #{thisword.downcase}\n")
+    end
+  end
+end
+text3.each do |thisline|
   if thisline =~ /^\w/
     thisarray = thisline.split(/\s+/)
     thisword = thisarray[0]
